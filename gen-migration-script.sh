48b2152e3c3e03906252d5be6e4bc9578a0be086
Rob Anderson <robandpdx@github.com>
2022-07-12 13:40:09 -0500

    only use GHES options for GHES

--- gen-migration-script.sh
+++ gen-migration-script.sh
@@ -4,5 +4,11 @@
 
 FILENAME="migration-script.sh"
 
+if [ -z "$GHES_API_URL" ]; then
+    GHES_INCLUDE=""
+else
+    GHES_INCLUDE="--ghes-api-url $GHES_API_URL --azure-storage-connection-string $AZURE_STORAGE_CONNECTION_STRING"
+fi
+
 gh gei generate-script --github-source-org $SOURCE_ORG --github-target-org $DESTINATION_ORG --output $FILENAME \
-   --ghes-api-url $GHES_API_URL --azure-storage-connection-string $AZURE_STORAGE_CONNECTION_STRING
+   $GHES_INCLUDE --download-migration-logs
--- migrate-repo.sh
+++ migrate-repo.sh
@@ -2,5 +2,11 @@
 
 . setenv.sh
 
+if [ -z "$GHES_API_URL" ]; then
+    GHES_INCLUDE=""
+else
+    GHES_INCLUDE="--ghes-api-url $GHES_API_URL --azure-storage-connection-string $AZURE_STORAGE_CONNECTION_STRING"
+fi
+
 gh gei migrate-repo --github-source-org $SOURCE_ORG --source-repo $CURRENT_REPO_NAME --github-target-org $DESTINATION_ORG --target-repo $NEW_REPO_NAME \
-    --ghes-api-url $GHES_API_URL --azure-storage-connection-string $AZURE_STORAGE_CONNECTION_STRING
+    $GHES_INCLUDE
