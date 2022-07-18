9ece13a556d567f8466dc2d10614467682b61729
Alejandro Menocal <amenocal@github.com>
2022-07-18 21:59:49 +0000

    uncomment actual migration script

--- migrate-repo.sh
+++ migrate-repo.sh
@@ -17,5 +17,5 @@ else
     GHES_INCLUDE="--ghes-api-url $GHES_API_URL --azure-storage-connection-string $AZURE_STORAGE_CONNECTION_STRING"
 fi
 
-#gh gei migrate-repo --github-source-org $SOURCE_ORG --source-repo $CURRENT_REPO_NAME --github-target-org $DESTINATION_ORG --target-repo $NEW_REPO_NAME \
-#    $GHES_INCLUDE
+gh gei migrate-repo --github-source-org $SOURCE_ORG --source-repo $CURRENT_REPO_NAME --github-target-org $DESTINATION_ORG --target-repo $NEW_REPO_NAME \
+    $GHES_INCLUDE
