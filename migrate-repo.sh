61251d0e9b444dbb0a639ac4224b8c27b89babf6
Alejandro Menocal <amenocal@github.com>
2022-07-18 21:41:04 +0000

    add option to supply repo names via cmd-line arg

--- migrate-repo.sh
+++ migrate-repo.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-
 . setenv.sh
+
+if [ -z "$CURRENT_REPO_NAME" ] ; then
+   CURRENT_REPO_NAME="$1"
+   echo "old repo: $CURRENT_REPO_NAME"
+fi
+
+if [ -z "$NEW_REPO_NAME" ] ; then
+   NEW_REPO_NAME="$2"
+    echo "new repo: $NEW_REPO_NAME"
+fi
 
 if [ -z "$GHES_API_URL" ]; then
     GHES_INCLUDE=""
@@ -8,5 +17,5 @@ else
     GHES_INCLUDE="--ghes-api-url $GHES_API_URL --azure-storage-connection-string $AZURE_STORAGE_CONNECTION_STRING"
 fi
 
-gh gei migrate-repo --github-source-org $SOURCE_ORG --source-repo $CURRENT_REPO_NAME --github-target-org $DESTINATION_ORG --target-repo $NEW_REPO_NAME \
-    $GHES_INCLUDE
+#gh gei migrate-repo --github-source-org $SOURCE_ORG --source-repo $CURRENT_REPO_NAME --github-target-org $DESTINATION_ORG --target-repo $NEW_REPO_NAME \
+#    $GHES_INCLUDE
