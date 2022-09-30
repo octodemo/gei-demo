77883834738111642e5f95cc39020dac8f162cc7
Rob Anderson <robandpdx@github.com>
2022-09-30 09:39:09 -0700

    use lock with gen script

--- gen-migration-script.sh
+++ gen-migration-script.sh
@@ -10,5 +10,11 @@ else
     GHES_INCLUDE="--ghes-api-url $GHES_API_URL --azure-storage-connection-string $AZURE_STORAGE_CONNECTION_STRING"
 fi
 
+if [ "$LOCK_SOURCE_REPO" = "true" ]; then
+    LOCK_SOURCE_REPO_FLAG="--lock-source-repo"
+else
+    LOCK_SOURCE_REPO_FLAG=""
+fi
+
 gh gei generate-script --github-source-org $SOURCE_ORG --github-target-org $DESTINATION_ORG --output $FILENAME \
-   $GHES_INCLUDE --download-migration-logs
+   $GHES_INCLUDE --download-migration-logs $LOCK_SOURCE_REPO_FLAG
