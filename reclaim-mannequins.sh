7e85e42fd2be5099bc0ba9f4a7754ddbc66b7bdc
Rob Anderson <robandpdx@github.com>
2024-06-20 20:29:34 +0200

    add skip invitations and update token to Bearer

--- reclaim-mannequins.sh
+++ reclaim-mannequins.sh
@@ -4,7 +4,7 @@
 
 FILENAME="usermapping.csv"
 
- if gh gei reclaim-mannequin --github-target-org $TARGET_ORG --csv $FILENAME; then
+ if gh gei reclaim-mannequin --github-target-org $TARGET_ORG --csv $FILENAME --skip-invitation; then
     echo "Mannequins reclaimed successfully"
 else
     echo "Mannequins not reclaimed successfully"
--- source-lock-remove.sh
+++ source-lock-remove.sh
@@ -5,11 +5,11 @@
 ID=`curl \
   -X GET \
   -H "Accept: application/vnd.github+json" \
-  -H "Authorization: token $GH_SOURCE_PAT" \
+  -H "Authorization: Bearer $GH_SOURCE_PAT" \
   $GHES_API_URL/orgs/$SOURCE_ORG/migrations | jq -r '.[0].id' | xargs echo`
 
 curl \
   -X DELETE \
   -H "Accept: application/vnd.github+json" \
-  -H "Authorization: token $GH_SOURCE_PAT" \
+  -H "Authorization: Bearer $GH_SOURCE_PAT" \
   $GHES_API_URL/orgs/$SOURCE_ORG/migrations/$ID/repos/$SOURCE_REPO_NAME/lock
